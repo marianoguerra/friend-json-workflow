eb8cefcd273de8355636184ad439d52c71cdad76
Mariano Guerra <luismarianoguerra@gmail.com>
2013-03-26 14:01:33 +0100

    bump version to 0.2.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.marianoguerra/friend-json-workflow "0.2.0-SNAPSHOT"
+(defproject org.marianoguerra/friend-json-workflow "0.2.0"
   :description "json auth workflow for cemerick/friend"
   :url "http://github.com/marianoguerra/friend-json-workflow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.1.3"]
-                 [com.cemerick/friend "0.1.3"]])
+                 [com.cemerick/friend "0.1.4"]])
